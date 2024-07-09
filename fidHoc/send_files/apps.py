dad7d4c54a0f6db1659ba8b1344694e8509c2b80
Shash <shashgk23@gmail.com>
2024-07-09 09:27:04 -0400

    made some changes to files

--- fidHoc/send_files/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SendFilesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'send_files'
--- fidHoc/send_files/views.py
+++ fidHoc/send_files/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from message.models import Message, File
 
 # Create your views here.
 def insert_file(request):
